f7a4997793451039c5079fd41756e87c9cd4aa27
bogdan-razvan <razvan@archlime.com>
2018-01-16 09:27:18 +0200

    add authentication

--- todos-api/spec/rails_helper.rb
+++ todos-api/spec/rails_helper.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
@@ -95,6 +95,8 @@ Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 # [...]
 RSpec.configure do |config|
   # [...]
-  config.include RequestSpecHelper, type: :request
+  #  config.include RequestSpecHelper, type: :request
+  config.include RequestSpecHelper
+  config.include ControllerSpecHelper
   # [...]
 end
